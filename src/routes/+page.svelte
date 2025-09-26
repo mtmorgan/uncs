54a07e7b2161651342e394e98e224dc31773b39c
Martin Morgan <mtmorgan.xyz@gmail.com>
2025-09-26 10:06:13 -0400

    Improve use of sveltestrap
    - use '<Container />' for padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,46 @@
 <script>
-  import { Styles, Accordion, AccordionItem } from "@sveltestrap/sveltestrap";
+  import { Styles, Container } from "@sveltestrap/sveltestrap";
   import People from "./People.svelte";
   import Ancestry from "./Ancestry.svelte";
   import Geography from "./Geography.svelte";
-  let id = 1;
-  let open = true;
 </script>
 
 <Styles />
 
-<h1>Uncs</h1>
+<Container fluid>
 
-<p>
-  'Uncs' is, apparently, a term used by the younger peoples to refer to older
-  individuals with unappreciated wisdom.
-</p>
+  <h1>Uncs</h1>
 
-<p>This project looks at our family genealogical history.</p>
+  <p>
+    'Uncs' is, apparently, a term used by the younger peoples to refer to older
+    individuals with unappreciated wisdom.
+  </p>
 
-<h2>People</h2>
+  <p>This project looks at our family genealogical history.</p>
 
-<People />
+  <h2>People</h2>
 
-<h2>Ancestry</h2>
+  <People />
 
-<Ancestry />
+  <h2>Ancestry</h2>
+
+  <Ancestry />
 
 <h2>Geographical genealogy</h2>
 
-<Geography />
+  <Geography />
 
-<h2>Implementation notes</h2>
+  <h2>Implementation notes</h2>
 
-<p>Ancestry information was collated by Rachel.</p>
+  <p>Ancestry information was collated by Rachel.</p>
 
-<p>
-  This application uses the <a href="https://svelte.dev">svelte</a>
-  framework. Data are displayed using
-  <a href="https://datatables.net/">DataTables</a>. The ancestry graph uses
-  <a href="https://graphology.github.io/">graphology</a>
-  and
-  <a href="https://www.sigmajs.org/">Sigma.js</a>.
-</p>
+  <p>
+    This application uses the <a href="https://svelte.dev">svelte</a>
+    framework. Data are displayed using
+    <a href="https://datatables.net/">DataTables</a>. The ancestry graph uses
+    <a href="https://graphology.github.io/">graphology</a>
+    and
+    <a href="https://www.sigmajs.org/">Sigma.js</a>.
+  </p>
+
+</Container>
